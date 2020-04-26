25df02891302a7cd77474072475d738f7e95714c
malc0mn <malc0mn@users.noreply.github.com>
2020-04-26 10:49:42 +0200

    Added PTP vendor extensions.

---
+++ ptp/consts/vendors.go
@@ -0,0 +1,24 @@
+package ptp
+
+type VendorExtension uint32
+
+const (
+	VE_EastmanKodakCompany VendorExtension = 0x00000001
+	VE_SeikoEpson VendorExtension = 0x00000002
+    VE_AgilentTechnologiesInc VendorExtension = 0x00000003
+	VE_PolaroidCorporation VendorExtension = 0x00000004
+	VE_AgfaGevaert VendorExtension = 0x00000005
+	VE_MicrosoftCorporation VendorExtension = 0x00000006
+	VE_EquinoxResearchLtd VendorExtension = 0x00000007
+	VE_ViewQuestTechnologies VendorExtension = 0x00000008
+    VE_STMicroelectronics VendorExtension = 0x00000009
+	VE_NikonCorporation VendorExtension = 0x0000000A
+	VE_CanonInc VendorExtension = 0x0000000B
+	VE_FotoNationInc VendorExtension = 0x0000000C
+	VE_PENTAXCorporation VendorExtension = 0x0000000D
+	VE_FujiPhotoFilmCoLtd VendorExtension = 0x0000000E
+	VE_NddMedicalTechnologies VendorExtension = 0x00000012
+	VE_SamsungElectronicsCoLtd VendorExtension = 0x0000001A
+	VE_ParrotDronesSAS VendorExtension = 0x0000001B
+	VE_PanasonicCorporation VendorExtension = 0x0000001C
+)
