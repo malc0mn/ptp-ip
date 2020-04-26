45abed6264d42afb14a43b0ce5152bf350070cf3
malc0mn <malc0mn@users.noreply.github.com>
2020-04-26 18:52:41 +0200

    Added package docs.

---
+++ ptp/doc.go
@@ -0,0 +1,7 @@
+/*
+	Package ptp implements the Picture Transfer Protocol (ISO-15740) basics in a transport agnostic way.
+	The structs and constants it exposes can be used in various implementations.
+
+	TODO: complete docs.
+*/
+package ptp
