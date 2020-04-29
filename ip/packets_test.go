a66a0c98bd471978d030ece4be69b8c797b3dd5b
malc0mn <malc0mn@users.noreply.github.com>
2020-04-29 11:29:19 +0200

    Go format.

--- ip/packets_test.go
+++ ip/packets_test.go
@@ -8,19 +8,19 @@ import (
 func TestNewPacketFromPacketType(t *testing.T) {
 	types := map[PacketType]string{
 		PKT_InitCommandRequest: "InitCommandRequest",
-		PKT_InitCommandAck: "InitCommandAck",
-		PKT_InitEventRequest: "InitEventRequest",
-		PKT_InitEventAck: "InitEventAck",
-		PKT_InitFail: "InitFail",
-		PKT_OperationRequest: "OperationRequest",
-		PKT_OperationResponse: "OperationResponse",
-		PKT_Event: "Event",
-		PKT_StartData: "StartData",
-		PKT_Data: "Data",
-		PKT_Cancel: "Cancel",
-		PKT_EndData: "EndData",
-		PKT_ProbeRequest: "ProbeRequest",
-		PKT_ProbeResponse: "ProbeResponse",
+		PKT_InitCommandAck:     "InitCommandAck",
+		PKT_InitEventRequest:   "InitEventRequest",
+		PKT_InitEventAck:       "InitEventAck",
+		PKT_InitFail:           "InitFail",
+		PKT_OperationRequest:   "OperationRequest",
+		PKT_OperationResponse:  "OperationResponse",
+		PKT_Event:              "Event",
+		PKT_StartData:          "StartData",
+		PKT_Data:               "Data",
+		PKT_Cancel:             "Cancel",
+		PKT_EndData:            "EndData",
+		PKT_ProbeRequest:       "ProbeRequest",
+		PKT_ProbeResponse:      "ProbeResponse",
 	}
 
 	for typ, want := range types {
