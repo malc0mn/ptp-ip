8f60716cc30762005c21fcbf49facd8f3f4fb005
malc0mn <malc0mn@users.noreply.github.com>
2020-05-09 19:47:43 +0200

    Added client initialisation to CLI.

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/malc0mn/ptp-ip/ip"
 	"os"
 	"path/filepath"
 )
@@ -13,9 +14,9 @@ var (
 )
 
 func main() {
-	initFlags()
-
 	exe = filepath.Base(os.Args[0])
+
+	initFlags()
 
 	if noArgs := len(os.Args) < 2; noArgs || help == true {
 		usage()
@@ -29,6 +30,20 @@ func main() {
 	if version == true {
 		fmt.Printf("%s version %s built on %s\n", exe, Version, BuildTime)
 		os.Exit(0)
+	}
+
+	/*sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)*/
+
+	c, err := ip.NewClient(host, int(port), fname, guid)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating PTP/IP client: %s\n", err)
+	}
+	fmt.Printf("Created new client with name '%s' and GUID '%s'.\n", c.InitiatorFriendlyName(), c.InitiatorGUIDAsString())
+	fmt.Printf("Attempting to connect to: %s\n", c.String())
+	err = c.Dial()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating PTP/IP client: %s\n", err)
 	}
 
 	if server == true {
