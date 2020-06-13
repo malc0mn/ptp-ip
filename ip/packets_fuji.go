4cf3722f80e027f9804cd051c3bf2b556871815e
malc0mn <malc0mn@users.noreply.github.com>
2020-06-13 14:56:02 +0200

    More documentation work.

--- ip/packets_fuji.go
+++ ip/packets_fuji.go
@@ -10,13 +10,13 @@ import (
 )
 
 const (
-	// This property indicates the initialisation sequence being used. It MUST be set by the Initiator during the
-	// initialisation sequence and depending on it's value, will require a different init sequence to be used.
+	// DPC_Fuji_UseInitSequence indicates the initialisation sequence being used. It MUST be set by the Initiator during
+	// the initialisation sequence and depending on it's value, will require a different init sequence to be used.
 	// See PM_Fuji_InitSequence for further info.
 	DPC_Fuji_UseInitSequence ptp.DevicePropCode = 0xDF01
-	// This property indicates the minium application version the camera will accept. It MUST be set by the Initiator
-	// during the initialisation sequence. As soon as this is done, the camera will acknowledge the client and store the
-	// client's friendly name to allow future connections without the need for a confirmation.
+	// DPC_Fuji_AppVersion indicates the minium application version the camera will accept. It MUST be set by the
+	// Initiator during the initialisation sequence. As soon as this is done, the camera will acknowledge the client and
+	// store the client's friendly name to allow future connections without the need for a confirmation.
 	DPC_Fuji_AppVersion ptp.DevicePropCode = 0xDF24
 
 	DPC_Fuji_FilmSimulation     ptp.DevicePropCode = 0xD001
@@ -34,17 +34,18 @@ const (
 	DPC_Fuji_ImageAspect        ptp.DevicePropCode = 0xD241
 	DPC_Fuji_BatteryLevel       ptp.DevicePropCode = 0xD242
 
-	// This fail reason is returned in the following cases:
+	// FR_Fuji_DeviceBusy is returned in the following cases:
 	//   - The FriendlyName stored in the camera does not match the FriendlyName being sent. Set the camera to 'change'
 	//     so that it will accept a new FriendlyName
 	//   - The camera side has timed out waiting for a connection and displays 'not found'. Set the camera to 'retry' to
 	//     allow resending the InitCommandRequestPacket.
 	// Seems to be an own version of RC_DeviceBusy.
 	FR_Fuji_DeviceBusy FailReason = 0x00002019
-	// This error is returned when the InitCommandRequestPacket has the wrong protocol version.
+	// FR_Fuji_InvalidParameter is returned when the InitCommandRequestPacket has the wrong protocol version.
 	// Seems to be an own version of RC_InvalidParameter.
 	FR_Fuji_InvalidParameter FailReason = 0x0000201D
 
+	// OC_Fuji_GetDeviceInfo is not really the same as OC_GetDeviceInfo in that it returns a list of device
 	OC_Fuji_GetDeviceInfo ptp.OperationCode = 0x902B
 
 	// For convenience: to be used with FujiSendOperationRequest() when no parameter is required for the operation.
@@ -69,14 +70,16 @@ const (
 	// This is the Fuji Protocol Version required to construct a valid InitCommandRequestPacket.
 	PV_Fuji ProtocolVersion = 0x8F53E4F2
 
-	// The response code to a OC_GetDevicePropValue. The first parameter in the packet will hold the property value.
+	// RC_Fuji_DevicePropValue is the response code to a OC_GetDevicePropValue. The first parameter in the packet will
+	// hold the property value.
 	RC_Fuji_DevicePropValue = ptp.OperationResponseCode(ptp.OC_GetDevicePropValue)
+	// RC_Fuji_DeviceInfo is the response code to OC_Fuji_GetDeviceInfo.
 	RC_Fuji_DeviceInfo      = ptp.OperationResponseCode(OC_Fuji_GetDeviceInfo)
 )
 
-// The Fuji version of the PTP/IP InitCommandRequestPacket deviates from the standard. Looking at what is sent 'over the
-// wire', we see this sequence in little endian format as the START of the packet, right after the header fields, being
-// the Length (4 bytes) and PacketType (4 bytes) fields:
+// FujiInitCommandRequestPacket is the Fuji version of the PTP/IP InitCommandRequestPacket which deviates from the
+// standard. Looking at what is sent 'over the wire', we see this sequence in little endian format as the START of the
+// packet, right after the header fields, being the Length (4 bytes) and PacketType (4 bytes) fields:
 //   [20]byte{
 //       0xf2, 0xe4, 0x53, 0x8f,
 //       0xad, 0xa5, 0x48, 0x5d,
@@ -130,13 +133,13 @@ func NewFujiInitCommandRequestPacket(guid uuid.UUID, friendlyName string) InitCo
 	}
 }
 
-// Fuji does not require the Event channel to be initialised, so we return a nil value here. This will skip any further
-// Event channel initialisation.
+// NewFujiInitEventRequestPacket returns nil because Fuji does not require the Event channel to be initialised. This
+// will skip any further event channel initialisation.
 func NewFujiInitEventRequestPacket(connNum uint32) InitEventRequestPacket {
 	return nil
 }
 
-// The Fuji OperationRequestPacket deviates from the PTP/IP standard in several ways:
+// FujiOperationRequestPacket deviates from the PTP/IP standard in several ways:
 //   - the packet type should be PKT_OperationRequest, but there is NO packet type sent out in the packet header (which
 //     is really annoying)!
 //   - the DataPhase should be uint32 but Fuji uses uint16
@@ -159,7 +162,7 @@ func (forp *FujiOperationRequestPacket) Payload() []byte {
 	return ipInternal.MarshalLittleEndian(forp)
 }
 
-// The Fuji OperationResponsePacket deviates from the PTP/IP standard similarly to the Fuji OperationRequestPacket:
+// FujiOperationResponsePacket deviates from the PTP/IP standard similarly to FujiOperationRequestPacket:
 //   - the packet type should be PKT_OperationResponse, but there is NO packet type sent out in the packet header which
 //     is, as one can imagine, extremely annoying when parsing the TCP/IP data coming in.
 //   - the DataPhase should be uint32 but Fuji uses uint16
@@ -190,24 +193,26 @@ func (forp *FujiOperationResponsePacket) ReasonAsError() error {
 	return errors.New(ptp.OperationResponseCodeAsString(forp.OperationResponseCode))
 }
 
-// The PTP/IP protocol specifies how to set up the Command/Data connection which should immediately be followed by
-// setting up the Event connection. However Fuji wants additional communications before it is satisfied that the
-// Command/Data connection is properly setup. This additional initialisation is performed here.
+// FujiInitCommandDataConn initialises the Fuji command/data connection.
+// The PTP/IP protocol specifies how to set up the command/data connection which should immediately be followed by
+// setting up the event connection. However Fuji wants additional communications before it is satisfied that the
+// command/data connection is properly setup. This additional initialisation is performed here.
 // The sequence is as follows:
-//   - Open a session.
-//   - Set device property DPC_Fuji_UseInitSequence to the correct number of the init sequence being used by the
-//     Initiator.
-//   - If the client name differs from the one stored, the Responder will now prompt the user to acknowledge the client
-//     connection, displaying the client name that was communicated using the InitCommandRequestPacket.
-//   - We will wait for 30 seconds for an acknowledgement from the Responder which means the user has pressed the 'OK'
-//     on the camera.
-//   - Next we will request the value of device property DPC_Fuji_AppVersion which holds the current minimal application
-//     version supported by the Responder and we will simply acknowledge it by setting it to the same value.
-//     This way we will always support any future versions as required by the firmware; unless of course a newer init
-//     sequence should be required.
-//   - Finally, we send the operation request OC_InitiateOpenCapture which makes the Responder hand over control to the
-//     Initiator. This also opens up the event connection port used by Fuji on port 55741 so we can connect to it and
-//     complete the init sequence there.
+//   1. Open a session.
+//   2. Set device property DPC_Fuji_UseInitSequence to the correct number of the init sequence being used by the
+//      Initiator.
+//   3. If the client name differs from the one stored, the Responder will now prompt the user to acknowledge the client
+//      connection, displaying the client name that was communicated using the InitCommandRequestPacket.
+//   4. We will wait for 30 seconds for an acknowledgement from the Responder which means the user has pressed the 'OK'
+//      button on the camera.
+//   5. Next we will request the value of device property DPC_Fuji_AppVersion which holds the current minimal
+//      application version supported by the Responder and we will simply acknowledge it by setting it to the same
+//      value.
+//      This way we will always support any future versions as required by the firmware; unless of course a newer init
+//      sequence should be required.
+//   6. Finally, we send the operation request OC_InitiateOpenCapture which makes the Responder hand over control to the
+//      Initiator. This also opens up the event connection port used by Fuji on port 55741 so we can connect to it and
+//      complete the init sequence there.
 func FujiInitCommandDataConn(c *Client) error {
 	// The first part of the sequence is according to the PTP/IP standard, save for the different packet format.
 	if err := GenericInitCommandDataConn(c); err != nil {
