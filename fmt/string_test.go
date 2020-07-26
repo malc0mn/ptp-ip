89d71f6ae18e52db9d22d38f139e923d3b52ed84
malc0mn <malc0mn@users.noreply.github.com>
2020-07-26 13:52:58 +0200

    Added more tests to fmt package.

--- fmt/string_test.go
+++ fmt/string_test.go
@@ -1,6 +1,19 @@
 package fmt
 
-import "testing"
+import (
+	"github.com/malc0mn/ptp-ip/ip"
+	"github.com/malc0mn/ptp-ip/ptp"
+	"testing"
+)
+
+func TestConvertToHexString(t *testing.T) {
+	want := "0x501f"
+	got := ConvertToHexString(ptp.DPC_CopyrightInfo)
+
+	if got != want {
+		t.Errorf("HexStringToUint64() got = %s; want %s", got, want)
+	}
+}
 
 func TestHexStringToUint64(t *testing.T) {
 	got, err := HexStringToUint64("0x10G4", 13)
@@ -33,5 +46,63 @@ func TestHexStringToUint64(t *testing.T) {
 	wantI = uint64(4100)
 	if got != wantI {
 		t.Errorf("HexStringToUint64() return = %d; want %d", got, wantI)
+	}
+}
+
+func TestDevicePropCodeAsString(t *testing.T) {
+	want := "ISO"
+	got := DevicePropCodeAsString(ip.DPC_Fuji_ExposureIndex)
+	if got != want {
+		t.Errorf("DevicePropCodeAsString() got = %s; want %s", got, want)
+	}
+
+	got = DevicePropCodeAsString(ptp.DPC_ExposureIndex)
+	if got != want {
+		t.Errorf("DevicePropCodeAsString() got = %s; want %s", got, want)
+	}
+}
+
+func TestPropNameToDevicePropCode(t *testing.T) {
+	want := ip.DPC_Fuji_ExposureIndex
+	got, err := PropNameToDevicePropCode(ptp.VE_FujiPhotoFilmCoLtd, "iso")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got != want {
+		t.Errorf("PropNameToDevicePropCode() got = %#x; want %#x", got, want)
+	}
+
+	want = ptp.DPC_ExposureIndex
+	got, err = PropNameToDevicePropCode(ptp.VendorExtension(0), "iso")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got != want {
+		t.Errorf("PropNameToDevicePropCode() got = %#x; want %#x", got, want)
+	}
+
+	wantE := "unknown field name 'testing123'"
+	got, err = PropNameToDevicePropCode(ptp.VendorExtension(0), "testing123")
+	if err.Error() != wantE {
+		t.Errorf("PropNameToDevicePropCode() err = %s; want %s", err, wantE)
+	}
+	if got != 0 {
+		t.Errorf("PropNameToDevicePropCode() got = %#x; want %#x", got, want)
+	}
+}
+
+func TestDevicePropValAsString(t *testing.T) {
+	want := "PRO Neg. Hi"
+	got := DevicePropValAsString(ptp.VE_FujiPhotoFilmCoLtd, ip.DPC_Fuji_FilmSimulation, 6)
+
+	if got != want {
+		t.Errorf("DevicePropValAsString() got = %s; want %s", got, want)
+	}
+
+	want = "center spot"
+	got = DevicePropValAsString(ptp.VendorExtension(0), ptp.DPC_FocusMeteringMode, 1)
+
+	if got != want {
+		t.Errorf("DevicePropValAsString() got = %s; want %s", got, want)
 	}
 }
