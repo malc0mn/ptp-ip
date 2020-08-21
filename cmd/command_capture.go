498f7e7a8cae508db93811ba9653e3f56ca892b5
malc0mn <malc0mn@users.noreply.github.com>
2020-08-21 16:43:33 +0200

    Added multi capture preview saving support.

--- cmd/command_capture.go
+++ cmd/command_capture.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"github.com/malc0mn/ptp-ip/ip"
 	"io/ioutil"
+	"path/filepath"
 	"strconv"
+	"strings"
+	"sync"
 )
 
 func init() {
@@ -23,34 +26,62 @@ func (capture) alias() []string {
 
 func (cap capture) execute(c *ip.Client, f []string) string {
 	amount := 1
-	hasIntArg := false
 	if len(f) >= 1 {
 		if val, err := strconv.Atoi(f[0]); err == nil {
 			amount = val
-			hasIntArg = true
+			f = f[1:] // drop processed amount argument
 		}
 	}
 
-	var img []byte
+	var (
+		imgs chan []byte
+		output string
+		wg sync.WaitGroup
+	)
+	if len(f) >= 1 {
+		imgs = make(chan []byte, 10)
+		var path string
+		if !cap.isView(f[0]) {
+			ext := filepath.Ext(f[0])
+			path = strings.TrimSuffix(f[0], ext) + "-%d" + ext
+		}
+
+		wg.Add(1)
+		go func() {
+			i := 1
+			for img := range imgs {
+				if path != "" {
+					file := fmt.Sprintf(path, i)
+					if err := ioutil.WriteFile(file, img, 0644); err != nil {
+						output += err.Error() + "\n"
+						continue
+					}
+					output += fmt.Sprintf("Image preview saved to %s\n", file)
+					i++
+				} else {
+					output += preview(img) + "\n"
+				}
+			}
+			wg.Done()
+		}()
+	}
+
+	// TODO: send fmt.Sprintf("capturing %d images", amount) to a channel for instant output to the end user.
 	for i := 0; i < amount; i++ {
+		// TODO: send fmt.Sprintf("capturing image %d", i) to a channel for instant output to the end user.
 		var err error
-		img, err = c.InitiateCapture()
+		img, err := c.InitiateCapture()
 		if err != nil {
 			return err.Error()
 		}
-		// TODO: add support to save/view ALL captured images!
+		if imgs != nil {
+			imgs <- img
+		}
 	}
-
-	if !hasIntArg && len(f) >= 1 {
-		if cap.isView(f[0]) {
-			return preview(img) + "\n"
-		}
-
-		if err := ioutil.WriteFile(f[0], img, 0644); err != nil {
-			return err.Error() + "\n"
-		}
-
-		return fmt.Sprintf("Image preview saved to %s\n", f[0])
+	if imgs != nil {
+		close(imgs)
+		wg.Wait()
+		return output
 	}
 
 	plural := ""
@@ -70,7 +101,7 @@ func (cap capture) help() string {
 		for i, arg := range args {
 			switch i {
 			case 0:
-				help += "\t- " + arg + ": an integer value to indicate the amount of captures to make\n\tOR\n"
+				help += "\t- " + arg + ": an integer value to indicate the amount of captures to make\n"
 			case 1:
 				help += "\t- " + `"` + arg + `" opens a window to display the capture preview if the camera returns it` + "\n\tOR\n"
 			case 2:
